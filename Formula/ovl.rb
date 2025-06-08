b6c6abec08a865358117abf78ed0442ff4a9c10e
Kubik <84036232+KodeurKubik@users.noreply.github.com>
2025-06-08 14:06:13 +0200

    Update ovl.rb to v0.1.3

--- Formula/ovl.rb
+++ Formula/ovl.rb
@@ -1,8 +1,8 @@
 class Ovl < Formula
   desc "OpenVoxel Launcher - Command-line interface for OpenVoxel Studios"
   homepage "https://github.com/OpenVoxelStudios/CLI"
-  url "https://github.com/OpenVoxelStudios/CLI/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "0414840eb4a9d179f79d8bb0ec4d686f9dc3beee4a9a13ddc8e34d8c6005cfc1"
+  url "https://github.com/OpenVoxelStudios/CLI/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "dfb406f56a2e15c31fc08e67925ae99602a980cb86a3450c76bf64c50118a4df"
   license "MIT"
   head "https://github.com/OpenVoxelStudios/CLI.git", branch: "main"
 
@@ -14,6 +14,6 @@ class Ovl < Formula
   end
 
   test do
-    assert_match "0.1.2", shell_output("#{bin}/ovl --version")
+    assert_match "0.1.3", shell_output("#{bin}/ovl --version")
   end
 end
